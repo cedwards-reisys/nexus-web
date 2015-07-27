502bfd203a1fce94fcb76846b6adf49a05cafea7
Chris Edwards <cedwards@reisystems.com>
2015-07-27 18:28:44 -0400

    override homepage title

--- app/sites/all/themes/nexus/template.php
+++ app/sites/all/themes/nexus/template.php
@@ -4,3 +4,11 @@
  * @file
  * template.php
  */
+
+function nexus_preprocess_page(&$vars){
+    $path = $_GET['q'];
+
+    if (strpos($path,'/') !== false) {
+        drupal_set_title('The Source for Federal Spending Data');
+    }
+}
