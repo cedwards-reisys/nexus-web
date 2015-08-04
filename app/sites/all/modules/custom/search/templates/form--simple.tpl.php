ed64998f28cc80f1bc087694b094e8d28fc195f7
Chris Edwards <cedwards@reisystems.com>
2015-08-04 12:10:23 -0400

    fluid layout

---
+++ app/sites/all/modules/custom/search/templates/form--simple.tpl.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-sm-12 col-xs-12 col-lg-12">
+        <div class="input-group">
+            <input type="text" class="form-control " placeholder="Start typing here to find agencies, vendors, and more...">
+              <span class="input-group-btn"><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> <span class="sr-only">Search</span></button></span>
+        </div>
+    </div>
+</div>
