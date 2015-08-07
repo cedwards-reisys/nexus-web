805f0e8d026b66ef92a81e86249664783934e33d
Chris Edwards <cedwards@reisystems.com>
2015-08-06 20:08:10 -0400

    theme update

--- app/sites/all/themes/nexus/template.php
+++ app/sites/all/themes/nexus/template.php
@@ -18,57 +18,5 @@ function nexus_preprocess_page(&$variables) {
     if (strpos($path,'node/1') !== false) {
         drupal_set_title('The Source for Federal Spending Data');
     }
-
-    // Add information about the number of sidebars.
-    if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
-        $variables['content_column_class'] = ' class="col-sm-6"';
-    }
-    elseif (!empty($variables['page']['sidebar_first']) || !empty($variables['page']['sidebar_second'])) {
-        $variables['content_column_class'] = ' class="col-sm-9"';
-    }
-    else {
-        $variables['content_column_class'] = ' class="col-sm-12"';
-    }
-
-    // Primary nav.
-    $variables['primary_nav'] = FALSE;
-    if ($variables['main_menu']) {
-        // Build links.
-        $variables['primary_nav'] = menu_tree(variable_get('menu_main_links_source', 'main-menu'));
-        // Provide default theme wrapper function.
-        $variables['primary_nav']['#theme_wrappers'] = array('menu_tree__primary');
-    }
-
-    // Secondary nav.
-    $variables['secondary_nav'] = FALSE;
-    if ($variables['secondary_menu']) {
-        // Build links.
-        $variables['secondary_nav'] = menu_tree(variable_get('menu_secondary_links_source', 'user-menu'));
-        // Provide default theme wrapper function.
-        $variables['secondary_nav']['#theme_wrappers'] = array('menu_tree__secondary');
-    }
-
-    $variables['navbar_classes_array'] = array('navbar');
-
-    if (theme_get_setting('bootstrap_navbar_position') !== '') {
-        $variables['navbar_classes_array'][] = 'navbar-' . theme_get_setting('bootstrap_navbar_position');
-    }
-    else {
-        $variables['navbar_classes_array'][] = 'container-fluid';
-    }
-    if (theme_get_setting('bootstrap_navbar_inverse')) {
-        $variables['navbar_classes_array'][] = 'navbar-inverse';
-    }
-    else {
-        $variables['navbar_classes_array'][] = 'navbar-default';
-    }
 }
 
-/**
- * Implements hook_process_page().
- *
- * @see page.tpl.php
- */
-function nexus_process_page(&$variables) {
-    $variables['navbar_classes'] = implode(' ', $variables['navbar_classes_array']);
-}
