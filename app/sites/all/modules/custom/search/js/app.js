c5e144de327bf468b0dd40fe4820469f527d08f9
Chris Edwards <cedwards@reisystems.com>
2015-08-10 16:09:58 -0400

    enabled filters: recipient name and award type

--- app/sites/all/modules/custom/search/js/app.js
+++ app/sites/all/modules/custom/search/js/app.js
@@ -10,6 +10,33 @@
 
         init:function () {
 
+        },
+
+        getAwardTypes: function () {
+            return [
+                'A: BPA CALL',
+                'A: GWAC',
+                'B: IDC',
+                'BOA Basic Ordering Agreement',
+                'BPA Blanket Purchase Agreement',
+                'BPA Call Blanket Purchase Agreement',
+                'B: PURCHASE ORDER',
+                'C: DELIVERY ORDER',
+                'C: FSS',
+                'D: BOA',
+                'DCA Definitive Contract',
+                'D: DEFINITIVE CONTRACT',
+                'DO Delivery Order',
+                'E: BPA',
+                'F: COOPERATIVE AGREEMENT',
+                'FSS Federal Supply Schedule"',
+                'G: GRANT FOR RESEARCH',
+                'GWAC Government Wide Acquisition Contract',
+                'IDC Indefinite Delivery Contract',
+                'PO Purchase Order',
+                'S: FUNDED SPACE ACT AGREEMENT',
+                'T: TRAINING GRANT'
+            ];
         }
 
     });
--- app/sites/all/modules/custom/search/templates/results.tpl.php
+++ app/sites/all/modules/custom/search/templates/results.tpl.php
@@ -41,25 +41,41 @@
     <div class="col-md-2">
         <h3>Filter</h3>
 
-        <div id="searchFilterPanel" class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+        <div id="searchFilterPanel" class="panel-group" role="tablist" aria-multiselectable="true">
             <div class="panel panel-default">
                 <div class="panel-heading" role="tab" id="headingOne">
                     <h4 class="panel-title">
-                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <a role="button" data-toggle="collapse" data-parent="#searchFilterPanel" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                             Amount
                         </a>
                     </h4>
                 </div>
                 <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-                    <div class="panel-body">
+                    <div class="panel-body" style="padding-left: 5px; padding-right: 5px;">
 
-                        <form class="form-inline">
-                            <div class="form-group">
-                                <label class="sr-only" for="exampleInputAmount">Amount (in dollars)</label>
-                                <div class="input-group">
-                                    <div class="input-group-addon">$</div>
-                                    <input type="text" class="form-control" id="exampleInputAmount" placeholder="Amount">
-                                    <div class="input-group-addon">.00</div>
+                        <form class="form-horizontal">
+                            <div class="form-group form-group-sm">
+                                <div class="col-md-7" style="padding-left: 5px; padding-right: 5px;">
+                                    <select class="form-control input-sm">
+                                        <option></option>
+                                        <option value="<">Less than</option>
+                                        <option value=">">Greater than</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-5" style="padding-left: 5px; padding-right: 5px;">
+                                    <input type="text" class="form-control input-sm" id="exampleInputAmount" placeholder="Amount">
+                                </div>
+                            </div>
+                            <div class="form-group form-group-sm">
+                                <div class="col-md-7" style="padding-left: 5px; padding-right: 5px;">
+                                    <select class="form-control input-sm">
+                                        <option></option>
+                                        <option value="<">Less than</option>
+                                        <option value=">">Greater than</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-5" style="padding-left: 5px; padding-right: 5px;">
+                                    <input type="text" class="form-control input-sm" id="exampleInputAmount" placeholder="Amount">
                                 </div>
                             </div>
                         </form>
@@ -70,7 +86,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading" role="tab" id="headingTwo">
                     <h4 class="panel-title">
-                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#searchFilterPanel" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                             Award ID
                         </a>
                     </h4>
@@ -86,7 +102,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading" role="tab" id="headingThree">
                     <h4 class="panel-title">
-                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#searchFilterPanel" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                             Award Date
                         </a>
                     </h4>
@@ -111,49 +127,29 @@
             <div class="panel panel-default">
                 <div class="panel-heading" role="tab" id="headingFour">
                     <h4 class="panel-title">
-                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#searchFilterPanel" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
                             Recipient
                         </a>
                     </h4>
                 </div>
                 <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
                     <div class="panel-body">
-
-                        <input type="text" class="form-control" placeholder="Recipient Name">
-
+                        <input type="text" id="recipientNameInput" class="form-control" placeholder="Recipient Name">
                     </div>
                 </div>
             </div>
             <div class="panel panel-default">
                 <div class="panel-heading" role="tab" id="headingFive">
                     <h4 class="panel-title">
-                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#searchFilterPanel" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
                             Award Type
                         </a>
                     </h4>
                 </div>
                 <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
                     <div class="panel-body">
-
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" value="">
-                                DO Delivery Order
-                            </label>
-                        </div>
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" value="">
-                                PO Purchase Order
-                            </label>
-                        </div>
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" value="">
-                                DCA Definitive Contract
-                            </label>
-                        </div>
-
+                        <form id="awardTypeInput">
+                        </form>
                     </div>
                 </div>
             </div>
@@ -165,19 +161,35 @@
     <div class="col-md-9">
         <h3>Results</h3>
 
-        <table id="searchResults" class="table table-striped table-bordered" cellspacing="0" width="100%">
-            <thead>
-            <tr>
-                <th>Recipient</th>
-                <th>Award ID</th>
-                <th>Award Amount</th>
-                <th>Award Date</th>
-                <th>Award Type</th>
-                <th>Awarding Agency</th>
-                <th>Funding Agency</th>
-            </tr>
-            </thead>
-        </table>
+        <div id="searchMessageWrapper">
+            <div id="searchErrorMessage" class="alert alert-danger alert-dismissible fade in" role="alert" style="display: none;">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+                <h4>Uh oh! Something went wrong!</h4>
+                <p>The search request could not be completed. Please choose from the following to continue searching:</p>
+                <p>
+                    <button type="button" class="btn btn-danger">Reload the page</button>
+                    <button type="button" class="btn btn-default">Or modify the search criteria</button>
+                </p>
+            </div>
+        </div>
+
+        <div id="searchTableWrapper">
+
+            <table id="searchResults" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                <thead>
+                <tr>
+                    <th>Recipient</th>
+                    <th>Award ID</th>
+                    <th>Award Amount</th>
+                    <th>Award Date</th>
+                    <th>Award Type</th>
+                    <th>Awarding Agency</th>
+                    <th>Funding Agency</th>
+                </tr>
+                </thead>
+            </table>
+
+        </div>
 
     </div>
 
@@ -190,25 +202,21 @@
 </div>
 
 
-
-
-
 <script type="text/javascript">
     jQuery(function() {
 
         var API_HOST = 'https://fedspending.demo.socrata.com/resource/nfu7-rhaq.json';
+        var ROWS_PER_PAGE = 20;
 
         var Uri = new FS.Util.UriHandler();
         jQuery('#searchInputKeywords').find('input').val(Uri.getParam('keywords'));
 
-        var ROWS_PER_PAGE = 20;
 
         var searchResultsTable = jQuery('#searchResults').dataTable( {
             processing: true,
             serverSide: true,
             searching: false,
             iDisplayLength: ROWS_PER_PAGE,
-            //pageLength: ROWS_PER_PAGE,
             bLengthChange: false,
             order: [[ 3,'desc']],
             language: {
@@ -262,10 +270,6 @@
                 var columnNames = ['vendorname','piid','dollarsobligated','signeddate','contractactiontype','agencyid','fundingrequestingagencyid'];
                 var order = columnNames[oSettings.aaSorting[0][0]] + ' ' + oSettings.aaSorting[0][1];
 
-                jQuery('#searchTransactionCount').find('dd').html('<div class="dataFetching"></div>');
-                jQuery('#searchContractCount').find('dd').html('<div class="dataFetching"></div>');
-                jQuery('#searchTransactionSum').find('dd').html('<div class="dataFetching"></div>');
-
                 var query = {
                     '$select': columnNames.join(','),
                     '$order': order,
@@ -273,17 +277,52 @@
                     '$limit': ROWS_PER_PAGE
                 };
 
+                // Keyword Input
                 var textSearch = jQuery('#searchInputKeywords').find('input').val();
                 if ( textSearch ) {
-
                     jQuery('#keywordsText').html(textSearch);
-
                     //query['$q'] = textSearch;
-                    query['$where'] = 'UPPER(vendorname) like \'%' + textSearch.replace(/'/g, "''").toUpperCase() + '%\'';
+                    query['$where'] = '(UPPER(vendorname) like \'%' + textSearch.replace(/'/g, "''").toUpperCase() + '%\'';
                     query['$where'] += ' OR UPPER(contractactiontype) like \'%' + textSearch.replace(/'/g, "''").toUpperCase() + '%\'';
-                    query['$where'] += ' OR UPPER(agencyid) like \'%' + textSearch.replace(/'/g, "''").toUpperCase() + '%\''
-                    query['$where'] += ' OR UPPER(fundingrequestingagencyid) like \'%' + textSearch.replace(/'/g, "''").toUpperCase() + '%\''
+                    query['$where'] += ' OR UPPER(agencyid) like \'%' + textSearch.replace(/'/g, "''").toUpperCase() + '%\'';
+                    query['$where'] += ' OR UPPER(fundingrequestingagencyid) like \'%' + textSearch.replace(/'/g, "''").toUpperCase() + '%\') ';
                 }
+
+                // Recipient Name Input
+                var recipientNameSearch = jQuery('#recipientNameInput').val();
+                if ( recipientNameSearch ) {
+                    if ( typeof query['$where'] === 'undefined' ) {
+                        query['$where'] = '';
+                    } else {
+                        query['$where'] += ' AND ';
+                    }
+                    query['$where'] += 'UPPER(vendorname) = \''+recipientNameSearch.toUpperCase()+'\' ';
+                }
+
+                // Award Type Input
+                var awardTypeSearch = jQuery('#awardTypeInput').find('input:checked');
+                if ( awardTypeSearch ) {
+                    var types = [];
+                    jQuery.each(awardTypeSearch,function(k,item){
+                        types.push('contractactiontype = \''+jQuery(item).val()+'\'');
+                    });
+
+                    if ( types.length ) {
+
+                        if (typeof query['$where'] === 'undefined') {
+                            query['$where'] = '';
+                        } else {
+                            query['$where'] += ' AND ';
+                        }
+
+                        query['$where'] += '('+types.join(' OR ')+')';
+                    }
+                }
+
+
+                jQuery('#searchTransactionCount').find('dd').html('<div class="dataFetching"></div>');
+                jQuery('#searchContractCount').find('dd').html('<div class="dataFetching"></div>');
+                jQuery('#searchTransactionSum').find('dd').html('<div class="dataFetching"></div>');
 
                 oSettings.jqXHR = jQuery.ajax({
                     dataType: 'json',
@@ -360,16 +399,12 @@
                         jQuery('#searchTransactionSum').find('dd').html('-');
                     }
                 }).fail(function (jqXHR, textStatus, errorThrown) {
-                    debugger;
 
-                    jQuery('#searchResults').dataTable().fnSettings().oLanguage.sEmptyTable = 'There was an error retrieving table data.';
-                    jQuery('#searchResults').dataTable().fnDraw();
+                    console.log(jqXHR);
 
-                    jQuery('#searchTransactionCount').find('dd').html('-');
-                    jQuery('#searchContractCount').find('dd').html('-');
-                    jQuery('#searchTransactionSum').find('dd').html('-');
+                    jQuery('#searchTableWrapper').hide();
+                    jQuery('#searchErrorMessage').show();
 
-                    //fnCallback({aaData:[],iTotalDisplayRecords:ROWS_PER_PAGE});
                 });
             }
         });
@@ -386,6 +421,38 @@
                 return false;
             }
         });
+
+        function initAwardTypeForm() {
+            var awardTypes = FS.SearchApp.getAwardTypes();
+            var html = '';
+            for ( var i = 0, len=awardTypes.length; i < len; i++ ) {
+                html += '<div class="checkbox input-sm">';
+                html += '  <label>';
+                html += '    <input type="checkbox" value="'+awardTypes[i]+'">';
+                html += '    '+awardTypes[i];
+                html += '  </label>';
+                html += '</div>';
+            }
+
+            var checkboxes = jQuery(html);
+            checkboxes.find('input[type=checkbox]').on('click',function(){
+                searchResultsTable.api().ajax.reload();
+            });
+
+            jQuery('#awardTypeInput').append(checkboxes);
+
+        }
+        initAwardTypeForm();
+
+
+        jQuery('#recipientNameInput').on('keyup',function(e){
+            var key = e.which;
+            if ( key == 13 ) {
+                searchResultsTable.api().ajax.reload();
+                return false;
+            }
+        });
+
 
     });
 
