e7958a658d9948fc6a47d1e5d2918f7ee106457b
Chris Edwards <cedwards@reisystems.com>
2015-08-18 18:49:56 -0400

    Update post-deployment.sh

--- scripts/post-deployment.sh
+++ scripts/post-deployment.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-ln -s /opt/nexus-web-storage/files /opt/nexus-web/app/sites/default/files
-ln -s /opt/nexus-web-storage/settings.php /opt/nexus-web/app/sites/default/settings.php
+
+if [ ! -L /opt/nexus-web/app/sites/default/files ]; then
+  ln -s /opt/nexus-web-storage/files /opt/nexus-web/app/sites/default/files
+fi
+
+if [ ! -L /opt/nexus-web/app/sites/default/settings.php ]; then
+  ln -s /opt/nexus-web-storage/settings.php /opt/nexus-web/app/sites/default/settings.php
+fi
+
 cd /opt/nexus-web/app
 drush cc all
