9aef65cf13d76e85a06177863e9d8e1bd468fc20
Chris Edwards <cedwards@reisystems.com>
2015-08-20 12:23:12 -0400

    theme updates

--- app/sites/all/modules/custom/search/templates/form--simple.tpl.php
+++ app/sites/all/modules/custom/search/templates/form--simple.tpl.php
@@ -1,34 +1,33 @@
-<div id="homepageMainSection">
-    <div id="homepageHeadline" class="col-md-4 col-md-offset-2">
-        <h1>The Offical<br>Government Spending</br>for Federal Spending Data</h1>
+<h1>The Offical Government Spending for Federal Spending Data</h1>
+
+<div class="homepage-search row">
+    <div class="col-md-4 col-md-offset-4">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search by keywords...">
+            <span class="input-group-btn"><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> <span class="sr-only">Search</span></button></span>
+        </div>
     </div>
+    <script type="text/javascript">
+        jQuery(function() {
 
-    <div id="homepageSearch" class="input-group col-md-3 col-md-offset-6">
-        <input type="text" class="form-control" placeholder="Search by keywords...">
-        <span class="input-group-btn"><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> <span class="sr-only">Search</span></button></span>
-    </div>
-</div>
-
-<script type="text/javascript">
-    jQuery(function() {
-
-        jQuery('#searchInputKeywords').find('button').on('click',function(){
-            var Uri = new FS.Util.UriHandler('/search');
-            var keywords = jQuery('#searchInputKeywords').find('input').val();
-
-            Uri.addParam('keywords',keywords).redirect();
-        });
-
-        jQuery('#searchInputKeywords').find('input').on('keyup',function(e){
-            var key = e.which;
-            if ( key == 13 ) {
+            jQuery('#searchInputKeywords').find('button').on('click',function(){
                 var Uri = new FS.Util.UriHandler('/search');
                 var keywords = jQuery('#searchInputKeywords').find('input').val();
 
                 Uri.addParam('keywords',keywords).redirect();
-                return false;
-            }
-        });
+            });
 
-    });
-</script>
+            jQuery('#searchInputKeywords').find('input').on('keyup',function(e){
+                var key = e.which;
+                if ( key == 13 ) {
+                    var Uri = new FS.Util.UriHandler('/search');
+                    var keywords = jQuery('#searchInputKeywords').find('input').val();
+
+                    Uri.addParam('keywords',keywords).redirect();
+                    return false;
+                }
+            });
+
+        });
+    </script>
+</div>
