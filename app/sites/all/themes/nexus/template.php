55fea1e9ab5b8cd0bc2adf5460085e07adacedef
Chris Edwards <cedwards@reisystems.com>
2015-08-27 15:13:37 -0400

    creating modal for feedback menu link

--- app/sites/all/themes/nexus/template.php
+++ app/sites/all/themes/nexus/template.php
@@ -14,13 +14,3 @@
 function nexus_preprocess_page(&$variables) {
 
 }
-
-function nexus_preprocess_menu_link(&$variables) {
-
-    // create modal for feedback menu link
-    if ( $variables['element']['#original_link']['link_path'] === 'node/14' ) {
-        $variables['element']['#localized_options']['attributes']['class'][] = 'ctools-use-modal';
-        $variables['element']['#localized_options']['attributes']['class'][] = 'ctools-modal-modal-popup-medium';
-        $variables['element']['#href'] = 'modal_forms/nojs/webform/13';
-    }
-}
